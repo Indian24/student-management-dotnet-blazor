a22dbb35ec83407d912806fc412e07016c6b2bec
Manish Ranjan <ranjanmanish516@gmail.com>
2026-03-12 14:07:26 +0530

    enable swagger for production

--- server/src/WebAPI/Program.cs
+++ server/src/WebAPI/Program.cs
@@ -14,23 +14,18 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Enable CORS for the client app running inside a Docker container
 app.UseCors(options => options.WithOrigins("http://localhost:8080").AllowAnyHeader().AllowAnyMethod());
-
-// Enable CORS for the client app running on localhost
 app.UseCors(options => options.WithOrigins("https://localhost:7147").AllowAnyHeader().AllowAnyMethod());
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", () => "Student Management API running on Railway 🚀");
 
 app.Run();
 
